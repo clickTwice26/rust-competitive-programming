10eb1f9bc23138967f4eaa7f29bd5e86ff6f2195
Shagato <47025267+clickTwice26@users.noreply.github.com>
2026-06-07 21:16:27 +0600

    chore: set up competitive programming rust workspace
    Set up root Cargo workspace for three competitive programming platforms: codeforces, leetcode, and cses. Add platform-specific Cargo.toml files and empty lib.rs for each subcrate. Include .gitignore with common Rust ignore patterns, a fast I/O competitive programming template, a comprehensive README with usage instructions, and sample problem binaries for each platform.

---
+++ cses/src/bin/weird_algorithm.rs
@@ -0,0 +1,33 @@
+use std::io::{self, BufWriter, Write, Read};
+
+fn solve<R: Read, W: Write>(inner_stdin: &mut R, inner_stdout: &mut W) {
+    let mut buffer = String::new();
+    inner_stdin.read_to_string(&mut buffer).unwrap();
+    let mut token_scanner = buffer.split_whitespace();
+
+    macro_rules! next {
+        ($t:ty) => {
+            token_scanner.next().unwrap().parse::<$t>().unwrap()
+        };
+    }
+
+    if let Some(n_str) = token_scanner.next() {
+        let n: usize = n_str.parse().unwrap();
+        let mut ans = 0;
+        for _ in 0..n {
+            let val: i64 = next!(i64);
+            ans += val;
+        }
+        writeln!(inner_stdout, "{}", ans).unwrap();
+    }
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let stdout = io::stdout();
+    let mut inner_stdin = stdin.lock();
+    let mut inner_stdout = BufWriter::new(stdout.lock());
+
+    solve(&mut inner_stdin, &mut inner_stdout);
+    inner_stdout.flush().unwrap();
+}
